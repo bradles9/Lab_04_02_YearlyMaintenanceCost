3cfb226e63f4f7cff4075162f38f62ed282ac272
bradles9 <bradles9@mail.uc.edu>
2025-02-03 16:11:15 -0500

    Yearly Maintenance

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+
+        double seasonOne=634.65;
+        double seasonTwo=453.44;
+        double seasonThree=777.77;
+        double seasonFour=876.99;
+        double yearlyMaintenanceCost;
+
+        yearlyMaintenanceCost=seasonFour+seasonThree+seasonTwo+seasonOne;
+
+        System.out.println("Maintenance Cost for season one is "); System.out.println(seasonOne);
+        System.out.println("Maintenance Cost for season two is "); System.out.println(seasonTwo);
+        System.out.println("Maintenance Cost for season three is "); System.out.println(seasonThree);
+        System.out.println("Maintenance Cost for season four is "); System.out.println(seasonFour);
+
+
+        System.out.println("The yearly maintenance costs are "); System.out.println(yearlyMaintenanceCost);
+
+
+
+
+
+
+    }
+}
